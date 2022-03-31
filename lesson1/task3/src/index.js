3d122e7424b5fa443b21a82ea6c122d3ca2edc90
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-03-31 12:38:35 +0800

    test

--- lesson1/task3/src/index.js
+++ lesson1/task3/src/index.js
@@ -3,25 +3,6 @@ import ReactDOM from 'react-dom';
 import './styles.css';
 const rootElement = document.querySelector('#root');
 
-/* const greetingElem = React.createElement(
-  'div',
-  { className: 'greeting' },
-  [
-    React.createElement(
-      'h1',
-      {className: 'greeting__title'},
-      'Hello, world!'
-    ),
-    React.createElement(
-      'p',
-      { className: 'greeting__text' },
-      "I'm learning React"
-    )
-  ]
-);
-
-ReactDOM.render(greetingElem, rootElement); */
-
 const element = (
   <div className="greeting">
     <div className="greeting__title">Hello, world</div>
