f5a9f797ae62ce6122a2fbdae8ca3513160cdf18
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-01 18:12:21 +0800

    test

--- lesson3/task3/src/index.jsx
+++ lesson3/task3/src/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./App.jsx";
+import Comment from "./comment.jsx";
 import './styles.scss';
 
 const rootElem = document.querySelector('#root');
 
+const userInfo = {
+  name: 'Tom',
+  avatarUrl: 'https://avatars1.githubusercontent.com'
+}
+
 ReactDOM.render(
-  <App/>, 
-  rootElem
+    <Comment 
+    user={userInfo} 
+    text='Good job!' 
+    date={new Date('2019-01-01T11:32:19.566Z')}
+    />,
+    rootElem
 );
