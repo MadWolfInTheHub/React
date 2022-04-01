e9165ca403f3bd089e213eebc48d588e6587ef78
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-01 20:32:45 +0800

    test

--- lesson3/task4/src/Greeting.jsx
+++ lesson3/task4/src/Greeting.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import moment from "moment";
+/* import moment from "moment";
 
-const formatDate = date => moment(date, "DD MMM YYYY").fromNow(true);
+const formatDate = date => moment(date, "DD MMM YYYY").fromNow(true); */
+const getAge = (date) => Math.floor((new Date() - new Date(date).getTime()) / (365 * 24 * 60 * 60 * 1000))
 
 const Greeting = (props) => {
   return (
     <div className="greeting">
-    My name is {props.firstName} {props.lastName}. I'm {formatDate(props.birthDate)} old
+    My name is {props.firstName} {props.lastName}. I'm {getAge(props.birthDate)} years old
     </div>
   );
 }
