07c1a1c73926c08189e9836d5a9a1542ca0e581c
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-02 10:42:20 +0800

    React jsx Profile

--- lesson3/home-work/webpack.config.js
+++ lesson3/home-work/webpack.config.js
@@ -3,13 +3,14 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const webpack = require("webpack");
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
+const path = require('path');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === "production";
   const config = {
     entry: "./src/index.jsx",
     output: {
-      filename: "bundle.js"
+      path: path.resolve(__dirname, 'review_build'),
     },
     module: {
       rules: [
