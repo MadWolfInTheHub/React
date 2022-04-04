1a909c112db90db41ba3f839d613f2b88aa970fa
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-04 10:59:57 +0800

    Argiments to the handler

---
+++ lesson5/task4/src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.scss'
+import Colors from "./Colors";
+
+const roootElem = document.querySelector('#root');
+
+ReactDOM.render(<Colors />, roootElem);
