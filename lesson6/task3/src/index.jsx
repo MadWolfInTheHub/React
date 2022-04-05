cc9c036f336d50c56aa6dfa297841f4acb905057
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-05 11:56:24 +0800

    Cancelation iof rendeding

---
+++ lesson6/task3/src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.scss';
+import Page from "./Page";
+
+const roootElem = document.querySelector('#root');
+
+ReactDOM.render(<Page/>, roootElem);
