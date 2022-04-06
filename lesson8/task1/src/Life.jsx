6129f7fa2c673c6312b484dafdbeea38188799f7
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-06 21:53:20 +0800

    test

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -10,16 +10,16 @@ class Life extends Component {
     console.log('componentDidMount: API calls, subscriptions');
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
-    return prevProps;
-  };
-
   shouldComponentUpdate(nextProps, nextState) {
     console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
     return nextProps.number % 2;
   };
   
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
+    return prevProps.number % 2;
+  };
+
   componentWillUnmount() {
     console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');
   };
