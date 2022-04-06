ae88481b433c6664aa724d35eb0c6dce51662971
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-06 11:58:59 +0800

    test

--- lesson7/task3/src/Transaction.js
+++ lesson7/task3/src/Transaction.js
@@ -12,8 +12,8 @@ const Transaction = ({ from, to, amount, rate, time }) => {
   return (
     <li className="transaction">
       <span className="transaction__date">{formatDate(time)}</span>
-      <span className="transaction__time">{from} - {to}</span>
-      <span className="transaction__assets">{formatTime(time)}</span>
+      <span className="transaction__time">{formatTime(time)}</span>
+      <span className="transaction__assets">{from} - {to}</span>
       <span className="transaction__rate">{rate}</span>
       <span className="transaction__amount">{formatAmount(amount)}</span>
     </li>
