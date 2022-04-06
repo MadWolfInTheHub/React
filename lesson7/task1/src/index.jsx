5a7543dba7f96f13e64fdad1034118aa13da89a1
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-06 09:28:49 +0800

    test

--- lesson7/task1/src/index.jsx
+++ lesson7/task1/src/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import './index.scss';
-import NumberList from './NumberList'
+import NumbersList from './NumbersList'
 
 const rootElem = document.querySelector('#root');
 const number = [1, 2, 3, 4, 5];
 
-ReactDOM.render(<NumberList numbers={number}/>, rootElem);
+ReactDOM.render(<NumbersList numbers={number}/>, rootElem);
