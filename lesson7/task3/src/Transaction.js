8c8c62a20cf2141e1506b854eca6a207695c788d
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-06 12:05:34 +0800

    test

--- lesson7/task3/src/Transaction.js
+++ lesson7/task3/src/Transaction.js
@@ -7,14 +7,12 @@ const formatTime = date => moment(date).format('hh:mm');
 const formatAmount = amount => new Intl.NumberFormat('en-GB', {maximumSignificantDigits: 3}).format(amount);
 
 const Transaction = ({ from, to, amount, rate, time }) => {
-  console.log(new Intl.NumberFormat('en-GB', {maximumSignificantDigits: 3}).format(amount))
-  console.log({ from, to, amount, rate, time });
   return (
     <li className="transaction">
       <span className="transaction__date">{formatDate(time)}</span>
       <span className="transaction__time">{formatTime(time)}</span>
       <span className="transaction__assets">{from} → {to}</span>
-      <span className="transaction__rate">{rate}</span>
+      <span className="transaction__rate">{formatAmount(rate)}</span>
       <span className="transaction__amount">{formatAmount(amount)}</span>
     </li>
   )
