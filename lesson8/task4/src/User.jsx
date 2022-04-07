dd2ce709feeb533f345dbe1bf9c3ec3202f0f9d0
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-07 13:18:54 +0800

    React loading data from server

--- lesson8/task4/src/User.jsx
+++ lesson8/task4/src/User.jsx
@@ -1,14 +1,46 @@
-<div class="user">
-  <!-- avatar_url -->
-  <img
-    alt="User Avatar"
-    src="https://avatars3.githubusercontent.com/u/69631?v=4"
-    class="user__avatar"
-  >
-  <div class="user__info">
-    <!-- name -->
-    <span class="user__name">Facebook</span>
-    <!-- location -->
-    <span class="user__location">Menlo Park, California</span>
-  </div>
-</div>
+import React, {Component} from 'react';
+
+class User extends Component {
+  state = {
+    user: null,
+  };
+
+  componentDidMount() {
+    this.fetchUser(this.props.userId)
+  }
+  
+  fetchUser = userId => {
+    fetch(`https://api.github.com/users/${userId}`)
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        user: data,
+      })
+    });
+  }
+
+  render() {
+    const { user } = this.state
+    if(!user) {
+      return null;
+    };
+
+    const{ avatar_url, name, location} = user;
+
+    return(
+      <div className="user">
+        <img
+          alt="User Avatar"
+          src={avatar_url}
+          className="user__avatar"
+        />
+        <div className="user__info">
+          <span className="user__name">{name}</span>
+          <span className="user__location">{location}</span>
+        </div>
+      </div>
+    );
+  };
+};
+
+export default User;
--- lesson8/task4/src/index.jsx
+++ lesson8/task4/src/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import User from './User';
 import './index.scss';
 
 const rootElem = document.querySelector('#root');
 
-ReactDOM.render(</>, rootElem);
+ReactDOM.render(<User userId='facebook'/>, rootElem);
