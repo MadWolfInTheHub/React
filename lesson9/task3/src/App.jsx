9d26eb1efb542a5e1a0dcdb796372531a442a6b4
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-08 12:06:37 +0800

    Uncontrolled component

---
+++ lesson9/task3/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import UserForm from './UserForm';
+
+class App extends Component {
+  render() {
+    return (
+      <UserForm />
+    );
+  };
+};
+
+export default App;
--- lesson9/task3/src/index.jsx
+++ lesson9/task3/src/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import './index.scss';
-import ConnectionStatus from "./ConnectionStatus";
+import App from "./App";
 
 const rootElem = document.querySelector('#root');
 
-ReactDOM.render(<ConnectionStatus />, rootElem);
+ReactDOM.render(<App />, rootElem);
