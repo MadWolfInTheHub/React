3fa4cd3cbcd9ab5990dd02a1d9c04f0a661c61fe
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-10 15:52:43 +0800

    Tic-tac-toe game

---
+++ lesson9/task5/src/index.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import './index.scss'
+
+import App from './App.jsx';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<App />, rootElement);
