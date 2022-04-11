acb1c69607107f8614dacdf5422d7f8eb89a3e18
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-11 13:05:50 +0800

    test

--- lesson9/task4/src/Filter.jsx
+++ lesson9/task4/src/Filter.jsx
@@ -3,15 +3,12 @@ class Filter extends Component {
   constructor(props) {
     super(props);
   };
-  state = {
-    text: this.props.filter
-  }
 
   render() {
     return(
       <>
         <span className="filter__count">{this.props.count}</span>
-        <input type="text" className="filter__input" onChange={this.props.onChange} value={this.state.text} />
+        <input type="text" className="filter__input" onChange={this.props.onChange} value={this.props.filter} />
       </>
     );
   };
