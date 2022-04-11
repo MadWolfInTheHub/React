9901246ed11d55aa6424feebd502e12f1285e776
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-11 17:00:42 +0800

    test

--- lesson10/task1/src/App.jsx
+++ lesson10/task1/src/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import UserProfile from './UserProfile';
 import UserMenu from "./UserMenu";
 
-class Page extends Component {
+class App extends Component {
   state = {
     user: null,
   };
@@ -33,4 +33,4 @@ class Page extends Component {
   };
 };
 
-export default Page;
+export default App;
--- lesson10/task1/src/index.jsx
+++ lesson10/task1/src/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Page from './App';
+import App from './App';
 import './index.scss';
 
 const rootElem = document.querySelector('#root');
 
-ReactDOM.render(<Page userId="facebook"/>, rootElem);
+ReactDOM.render(<App userId="facebook"/>, rootElem);
