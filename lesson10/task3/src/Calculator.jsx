4373abb2ce28f20066c4d57f6e9e3f6aa30a6372
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-12 11:56:45 +0800

    test

--- lesson10/task3/src/Calculator.jsx
+++ lesson10/task3/src/Calculator.jsx
@@ -6,65 +6,56 @@ class Calculator extends Component {
   constructor(props) {
     super(props);
     this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
-    this.handleFareinheitChange = this.handleFareinheitChange.bind(this);
-    this.state = {
-      temperature: '',
-      scale: 'c',
-    };
-  };
+    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+    this.state = { temperature: "", scale: "c" };
+  }
 
   handleCelsiusChange(temperature) {
-    this.setState({
-      scale: 'c',
-      temperature
-    });
-  };
+    this.setState({ scale: "c", temperature });
+  }
 
-  handleFareinheitChange(temperature) {
-    this.setState({
-      scale: 'f',
-      temperature
-    });
-  };
+  handleFahrenheitChange(temperature) {
+    this.setState({ scale: "f", temperature });
+  }
 
-  toCelsius(farenheit)  {
-    return (farenheit - 32) * 5 / 9;
+  toCelsius(fahrenheit) {
+    return ((fahrenheit - 32) * 5) / 9;
   }
   
-  toFarenheit(celsius) {
-    return (celsius * 9 / 5) + 32;
-
+  toFahrenheit(celsius) {
+    return (celsius * 9) / 5 + 32;
   }
   
   tryConvert(temperature, convert) {
     const input = parseFloat(temperature);
-    if(Number.isNaN(input)) {
-      return ''
+    if (Number.isNaN(input)) {
+      return "";
     }
     const output = convert(input);
-    const rounded = Math.round(output * 1000) /1000;
+    const rounded = Math.round(output * 1000) / 1000;
     return rounded.toString();
-  };
+  }
 
   render() {
     const scale = this.state.scale;
     const temperature = this.state.temperature;
-    const celsius = scale === 'f' ? this.tryConvert(temperature, this.toCelsius) : temperature;
-    const farenheit = scale === 'c' ? this.tryConvert(temperature, this.toFarenheit) : temperature;
+    const celsius =
+      scale === "f" ? this.tryConvert(temperature, this.toCelsius) : temperature;
+    const fahrenheit =
+      scale === "c" ? this.tryConvert(temperature, this.toFahrenheit) : temperature;
     return (
       <div>
-        <TemperatureInput 
+        <TemperatureInput
           scale="c"
           temperature={celsius}
-          onTemperautreChange={this.handleCelsiusChange}
+          onTemperatureChange={this.handleCelsiusChange}
         />
-        <TemperatureInput 
+        <TemperatureInput
           scale="f"
-          temperature={farenheit}
-          onTemperautreChange={this.handleFareinheitChange}
+          temperature={fahrenheit}
+          onTemperatureChange={this.handleFahrenheitChange}
         />
-        <BoilingVerdict
-          celsius={parseFloat(celsius)} />
+        <BoilingVerdict celsius={parseFloat(celsius)} />
       </div>
     );
   };
--- lesson10/task3/src/TemperatureInput.jsx
+++ lesson10/task3/src/TemperatureInput.jsx
@@ -9,22 +9,20 @@ class TemperatureInput extends Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
-  }
+  };
 
   handleChange(e) {
-    this.props.onTemperautreChange(e.target.value)
-  }
+    this.props.onTemperatureChange(e.target.value);
+  };
 
   render() {
     const temperature = this.props.temperature;
     const scale = this.props.scale;
     return (
-      <fildset>
+      <fieldset>
         <legend>Enter temperature in {scaleNames[scale]}:</legend>
-        <input value={temperature}
-          onChange={this.handleChange}
-        />
-      </fildset>
+        <input value={temperature} onChange={this.handleChange} />
+      </fieldset>
     );
   };
 };
