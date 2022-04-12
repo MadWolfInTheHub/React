7436467b60ab50045b4beb86d0ea1db4a20c4ab3
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-12 21:57:51 +0800

    test

--- lesson11/task3/src/App.jsx
+++ lesson11/task3/src/App.jsx
@@ -15,8 +15,9 @@ class App extends Component {
 
   render() {
     const elem = (
-      <p>Hooks are a new addition in React 16.8. They let you use state and other React features
-      without writing a class.
+      <p>
+        Hooks are a new addition in React 16.8. They let you use state and other React features
+        without writing a class.
       </p>
     )
     return (
