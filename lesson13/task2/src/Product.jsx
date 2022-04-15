fdce2f45fd4b2d00a6efef03525730ef05dc72c7
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-15 13:22:55 +0800

    test

--- lesson13/task2/src/Product.jsx
+++ lesson13/task2/src/Product.jsx
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
-const  Product = ({ match }) => {
+const  Product = () => {
+  const { productId } = useParams();
   return (
-    <div className='page__content'>
-      {`Product is a ${match.params.productId}`}
+    <div className='product'>
+      {productId}
     </div>
   );
  
