fa1cd092184b1dc62a172095c4a64d942a5d112c
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-15 19:05:54 +0800

    React Hooks - User Data

---
+++ lesson14/task2/src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.scss'
+import App from "./App";
+
+const roootElem = document.querySelector('#root');
+
+ReactDOM.render(<App />, roootElem);
--- lesson8/task4/src/User.jsx
+++ lesson8/task4/src/User.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-class User extends Component {
+class s Component {
   state = {
     user: null,
   };
