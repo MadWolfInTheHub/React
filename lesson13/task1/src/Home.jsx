9da2d30fc4d114e62901a29102843f4b523024c9
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-04-15 09:39:19 +0800

    React Router intro

---
+++ lesson13/task1/src/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+
+class Home extends Component {
+  render() {
+    return (
+      <div className='page__content'>
+        <h1>🏠</h1>
+        <Link to="/products">To Products</Link>
+      </div>
+    );
+  };
+};
+
+export default Home;
